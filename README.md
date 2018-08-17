0c86f8b953a08c22bafd560ed2c410d12137a727
Max Benson <max.benson@ahold.com>
2018-08-17 12:01:43 -0700

    Update environment variable keys ahead of deprecated web.

--- README.md
+++ README.md
@@ -2,174 +2,63 @@
 
 [Traefik](https://traefik.io) package for DC/OS.
 
-`traefik.toml` configuration file is generated using `confd` template. Currently configuration can be loaded from ENV variables, evetually support for consistent key-value storages like Etcd, ZooKeeper, Consul can be added.
+## Supported ENV variables
 
-## Quickstart
-
-In order to expose a service from Marathon add labels to your app:
-
-```
-labels: {
-  "traefik.enable": "true",
-  "traefik.frontend.rule": "Host:site.example.com",
-  "traefik.protocol": "http",
-  "traefik.backend.healthcheck.path": "/health",
-  "traefik.frontend.entryPoints":"http,https"
-}
-```
-Then you should be able to reach you site via Traefik's HTTP port (`TRAEFIK_HTTP_PORT`):
-```
-curl -H 'Host:site.example.com' localhost:80/health
-```
-
-See [documetation](https://docs.traefik.io/user-guide/marathon/) for more details.
-
-## Global configuration
-
- * `TRAEFIK_DEBUG` Default `false`
- * `TRAEFIK_INSECURE_SKIP` Default `false`
- * `TRAEFIK_LOG_LEVEL` Default `INFO`
- * `TRAEFIK_DEFAULT_ENTRYPOINTS` Entrypoints to be used by frontends that do not specify any entrypoint. Each frontend can specify its own entrypoints.
- * `TRAEFIK_GRACE_TIMEOUT` Duration to give active requests a chance to finish before Traefik stops (`10s`)
- * `TRAEFIK_HEALTHCHECK_INTERVAL` Set the default health check interval (`30s`).
- * `TRAEFIK_READ_TIMEOUT` The maximum duration for reading the entire request, including the body.
- * `TRAEFIK_WRITE_TIMEOUT` The maximum duration before timing out writes of the response.
- * `TRAEFIK_IDLE_TIMEOUT` The maximum duration an idle (keep-alive) connection will remain idle before closing itself.
- * `TRAEFIK_RETRY` Number of attempts to reach backend service.
-
-## Logs
-
-### Access logs
-
-By default the Traefik log is written to stdout in text format.
-
-* `TRAEFIK_ACCESS_LOG` Enable logging. Default `false`
-* `TRAEFIK_ACCESS_FORMAT` Logs format, e.g. `json`
-* `TRAEFIK_ACCESS_PATH` Path to log files.
-
-## Entrypoints
-
-### http
-
-* `TRAEFIK_HTTP_COMPRESSION` Default `true`
-* `TRAEFIK_HTTP_ADDRESS`
-* `TRAEFIK_HTTP_PORT` Default `80`
-
-### https
-
+ * `TRAEFIK_DCOS_CLUSTER_NAME` Default `dcos`
+ * `TRAEFIK_CONSUL_DOMAIN` Default `service.dev.localdomain`
+ * `TRAEFIK_HTTP_COMPRESSION` Default `true`
  * `TRAEFIK_HTTPS_COMPRESSION` Default `true`
+ * `TRAEFIK_HTTP_ADDRESS`
+ * `TRAEFIK_HTTP_PORT` Default `80`
  * `TRAEFIK_HTTPS_ENABLE` Default `false`
  * `TRAEFIK_HTTPS_ADDRESS`
  * `TRAEFIK_HTTPS_PORT` Default `443`
-
-### ping
-
-Heath check endpoint, responds without authentication to `/ping`.
-
- * `TRAEFIK_PING_ENABLE` Default `true`
- * `TRAEFIK_PING_PORT` If variable not defined, `$PORT0` will be used.
-
-### API
-
-Former `web.admin` interface.
-
- * `TRAEFIK_API_ENABLE` Enable API entrypoint
- * `TRAEFIK_API_PORT` If variable not defined, `$PORT1` will be used.
- * `TRAEFIK_API_DASHBOARD` Default `true`
- * `TRAEFIK_API_DEBUG` This will install HTTP handlers to expose Go expvars under /debug/vars and pprof profiling. Default `false`.
-
-## Custom configuration
-
-Appends custom configuration to generated `traefik.toml` config.
-
+ * `TRAEFIK_HTTPS_REDIRECT_ENABLE` Default `false`
+ * `TRAEFIK_API_ENABLE` Default `true`
+ * `TRAEFIK_API_PORT` Default `8080`
+ * `TRAEFIK_DEBUG` Default `false`
+ * `TRAEFIK_INSECURE_SKIP` Default `false`
+ * `TRAEFIK_LOG_LEVEL` Default `INFO`
+ * `TRAEFIK_STATISTICS_RECENT_ERRORS` Default `10`
+ * `TRAEFIK_API_AUTH_METHOD` Default `basic`
+ * `TRAEFIK_API_AUTH_USERS`
+ * `TRAEFIK_PING_PORT` Default `8081`
+ * `TRAEFIK_SSL_PATH` Default `$(pwd)/certs`
+ * `TRAEFIK_ACME_ENABLE` Certificates from Let's Encrypt. Default `false`
+ * `TRAEFIK_ACME_EMAIL` Default `test@traefik.io`
+ * `TRAEFIK_ACME_STORAGE` Default `$(pwd)/acme/acme.json`
+ * `TRAEFIK_ACME_ONHOSTRULE` Default `false`
+ * `TRAEFIK_ACME_CASERVER` Default `https://acme-v01.api.letsencrypt.org/directory`
+ * `TRAEFIK_ACME_DNS_PROVIDER` Default `-`
+ * `TRAEFIK_K8S_ENABLE` Default `false`
+ * `TRAEFIK_K8S_OPTS`
+ * `TRAEFIK_PROMETHEUS_ENABLE` Default `false`
+ * `TRAEFIK_PROMETHEUS_OPTS`
+ * `TRAEFIK_PROMETHEUS_ENTRYPOINT` Default `traefik`
+ * `TRAEFIK_PROMETHEUS_BUCKETS` Default `[0.1,0.3,1.2,5.0]`
+ * `TRAEFIK_RANCHER_ENABLE` Default `false`
+ * `TRAEFIK_RANCHER_REFRESH` Default `15`
+ * `TRAEFIK_RANCHER_MODE` Default `api`
+ * `TRAEFIK_RANCHER_DOMAIN` Default `rancher.internal`
+ * `TRAEFIK_RANCHER_EXPOSED` Default `false`
+ * `TRAEFIK_RANCHER_HEALTHCHECK` Default `true`
+ * `TRAEFIK_RANCHER_INTERVALPOLL` Default `false`
+ * `TRAEFIK_RANCHER_OPTS`
+ * `TRAEFIK_RANCHER_PREFIX` Default `/2017-11-11`
  * `TRAEFIK_FILE_NAME` Default `rules.toml`
  * `TRAEFIK_FILE_WATCH` Default `true`
-
-## Providers
-
-### Marathon
-
- * `TRAEFIK_MARATHON_ENABLE` Default `false`
+ * `CATTLE_URL`
+ * `CATTLE_ACCESS_KEY`
+ * `CATTLE_SECRET_KEY`
+ * `TRAEFIK_MARATHON_ENABLE` Default `true`
  * `TRAEFIK_MARATHON_ENDPOINT` `http://marathon.mesos:8080`
  * `TRAEFIK_MARATHON_WATCH` Default `true`
  * `TRAEFIK_MARATHON_LB_COMPATIBILITY` Default `false`
- * `TRAEFIK_MARATHON_DOMAIN` Default `marathon.localhost`
- * `TRAEFIK_MARATHON_EXPOSE` Expose Marathon apps by default in Traefik. Default `true`
- * `TRAEFIK_MARATHON_GROUPS_AS_SUBDOMAINS` Convert Marathon groups to subdomains. Default `false`
+ * `TRAEFIK_MARATHON_DOMAIN` Default `dcos.service.dev.localdomain`
+ * `TRAEFIK_MARATHON_OPTS`
+ * `TRAEFIK_MARATHON_EXPOSE` Default `true`
+ * `TRAEFIK_MARATHON_GROUPS_AS_SUBDOMAINS` Default `false`
  * `TRAEFIK_MARATHON_DIALER_TIMEOUT` Default `60s`
  * `TRAEFIK_MARATHON_KEEP_ALIVE` Default `10s`
  * `TRAEFIK_MARATHON_FORCE_TASK_HOSTNAME` Default `false`
  * `TRAEFIK_MARATHON_RESPECT_READINESS_CHECKS` Default `false`
-
-### Mesos
-
- * `TRAEFIK_MESOS_ENABLE` Default `false`
- * `TRAEFIK_MESOS_ENDPOINT` `http://leader.mesos:5050`
- * `TRAEFIK_MESOS_WATCH` Default `true`
- * `TRAEFIK_MESOS_EXPOSE` Expose Mesos apps by default in Traefik. Default `false`
- * `TRAEFIK_MESOS_FILENAME` Override default configuration template.
- * `TRAEFIK_MESOS_ZK_TIMEOUT` ZooKeeper timeout (in seconds).
- * `TRAEFIK_MESOS_REFRESH` Polling interval (in seconds).
- * `TRAEFIK_MESOS_IP_SOURCES` IP sources (e.g. host, docker, mesos, netinfo).
- * `TRAEFIK_MESOS_TIMEOUT` HTTP Timeout (in seconds).
- * `TRAEFIK_MESOS_GROUPS_AS_SUBDOMAINS` Convert Mesos groups to subdomains. Default `false`
-
-### Kubernetess
-
- * `TRAEFIK_K8S_ENABLE` Default `false`
- * `TRAEFIK_K8S_ENDPOINT` Kubernetes server endpoint.
- * `TRAEFIK_K8S_TOKEN` Bearer token used for the Kubernetes client configuration.
- * `TRAEFIK_K8S_CA` Path to the certificate authority file.
- * `TRAEFIK_K8S_NAMESPACES` Comma-separated namespaces to watch. e.g. `"\"default\",\"production\""`
- * `TRAEFIK_K8S_LABELSELECTOR` Ingress label selector to filter Ingress objects that should be processed. e.g. `"A and not B"`
- * `TRAEFIK_K8S_FILENAME` Override default configuration template.
- * `TRAEFIK_K8S_INGRESS_CLASS` Value of `kubernetes.io/ingress.class` annotation that identifies Ingress objects to be processed.
- * `TRAEFIK_K8S_DISABLE_PASS_HOST_HEADERS` Disable PassHost Headers.
- * `TRAEFIK_K8S_ENABLE_PASS_TLS_CERT` Enable PassTLSCert Headers.
-
-### Rancher
-
- * `TRAEFIK_RANCHER_ENABLE` Default `false`
- * `TRAEFIK_RANCHER_REFRESH` Default `15`
- * `TRAEFIK_RANCHER_MODE` Default `api`
- * `TRAEFIK_RANCHER_DOMAIN` Default `rancher.localhost`
- * `TRAEFIK_RANCHER_EXPOSE` Default `true`
- * `TRAEFIK_RANCHER_FILENAME` Configuration template.
- * `TRAEFIK_RANCHER_HEALTHCHECK` Filter services with unhealthy states and inactive states. Default `true`
- * `TRAEFIK_RANCHER_INTERVALPOLL` Default `false`
- * `TRAEFIK_RANCHER_PREFIX` Default `/latest`
-
-## Let's Encrypt
-
- * `TRAEFIK_ACME_ENABLE` Certificates from Let's Encrypt. Default `false`
- * `TRAEFIK_ACME_EMAIL`
- * `TRAEFIK_ACME_ENTRYPOINT` Default `https`.
- * `TRAEFIK_ACME_STORAGE` File or key used for certificates storage.
- * `TRAEFIK_ACME_ONHOSTRULE` Default `false`
- * `TRAEFIK_ACME_CASERVER` Default `https://acme-v01.api.letsencrypt.org/directory`
-
-## Metrics
-
-* `TRAEFIK_STATISTICS_RECENT_ERRORS` Number of recent errors logged. Default `10`.
-
-### DataDog
-
-* `TRAEFIK_DATADOG_ADDRESS` Enables sending metrics to DataDog, e.g. `localhost:8125`
-* `TRAEFIK_DATADOG_PUSHINTERVAL` Default `10s`
-
-### InfluxDB
-
-* `TRAEFIK_INFLUXDB_ADDRESS` Enables sending metrics to InfluxDB, e.g. `localhost:8089`
-* `TRAEFIK_INFLUXDB_PUSHINTERVAL` Default `10s`
-
-### Prometheus
-
-Will be enabled when `TRAEFIK_PROMETHEUS_ENTRYPOINT` is set, e.g. to `api`.
-
- * `TRAEFIK_PROMETHEUS_ENTRYPOINT`
- * `TRAEFIK_PROMETHEUS_BUCKETS` Comma separated values. Default `0.1,0.3,1.2,5.0`
-
-### StatsD
-
-* `TRAEFIK_STATSD_ADDRESS` Enables sending metrics to StatsD, e.g. `localhost:8125`
-* `TRAEFIK_STATSD_PUSHINTERVAL` Default `10s`
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,7 @@ export LC_ALL=C
 function -h {
   cat <<USAGE
    USAGE: Generates Traefik config
-   -b / --backend confd backend (configuration source)
-   -d / --debug   debugging output
+   -v / --verbose  debugging output
 USAGE
 }; function --help { -h ;}
 
@@ -14,31 +13,393 @@ function out { printf '%s\n' "$*" ;}
 function err { local x=$? ; msg "$*" ; return $(( $x == 0 ? 1 : $x )) ;}
 
 function main {
-  local debug=false
-  # confd backend (default: ENV variables)
-  local backend="env"
+  local verbose=false
   while [[ $# -gt 0 ]]
   do
     case "$1" in                                      # Munging globals, beware
-      -b|--backend)       backend=true; shift 1 ;;
-      -d|--debug)         debug=true; shift 1 ;;
+      -v|--verbose)         verbose=true; shift 1 ;;
       *)                    err 'Argument error. Please see help: -h' ;;
     esac
   done
-  if [[ $debug == true ]]; then
+  if [[ $verbose == true ]]; then
     set -ex
   fi
+  generate_config
   if [ -f traefik_linux-amd64 ]; then
     chmod +x traefik_linux-amd64
   fi
-  confd=$(ls -1 confd-*)
-  if [ -f "${confd}" ]; then
-    chmod +x ${confd}
-  fi
-  mkdir -p $(pwd)/{conf.d,templates}
-  msg "Generating Traefik configuration"
-  ./${confd} -onetime -backend ${backend} --confdir $(pwd)
 }
+
+function generate_config {
+  TRAEFIK_DCOS_CLUSTER_NAME=${TRAEFIK_DCOS_CLUSTER_NAME:-"dcos"}
+  TRAEFIK_CONSUL_DOMAIN=${TRAEFIK_CONSUL_DOMAIN:-"service.dev.localdomain"}
+  TRAEFIK_HTTP_COMPRESSION=${TRAEFIK_HTTP_COMPRESSION:-"true"}
+  TRAEFIK_HTTPS_COMPRESSION=${TRAEFIK_HTTPS_COMPRESSION:-"true"}
+  TRAEFIK_HTTP_ADDRESS=${TRAEFIK_HTTP_ADDRESS:-""}
+  TRAEFIK_HTTP_PORT=${TRAEFIK_HTTP_PORT:-"80"}
+  TRAEFIK_HTTPS_ENABLE=${TRAEFIK_HTTPS_ENABLE:-"false"}
+  TRAEFIK_HTTPS_ADDRESS=${TRAEFIK_HTTP_ADDRESS:-""}
+  TRAEFIK_HTTPS_PORT=${TRAEFIK_HTTPS_PORT:-"443"}
+  TRAEFIK_HTTPS_REDIRECT_ENABLE=${TRAEFIK_HTTPS_REDIRECT_ENABLE:-"false"}
+  TRAEFIK_API_ENABLE=${TRAEFIK_API_ENABLE:-"true"}
+  TRAEFIK_API_PORT=${TRAEFIK_API_PORT:-"8080"}
+  TRAEFIK_DEBUG=${TRAEFIK_DEBUG:="false"}
+  TRAEFIK_INSECURE_SKIP=${TRAEFIK_INSECURE_SKIP:="false"}
+  TRAEFIK_LOG_LEVEL=${TRAEFIK_LOG_LEVEL:-"INFO"}
+  TRAEFIK_STATISTICS_RECENT_ERRORS=${TRAEFIK_STATISTICS_RECENT_ERRORS:-10}
+  TRAEFIK_API_AUTH_METHOD=${TRAEFIK_API_AUTH_METHOD:-"basic"}
+  TRAEFIK_API_AUTH_USERS=${TRAEFIK_API_AUTH_USERS:-""}
+  TRAEFIK_PING_PORT=${TRAEFIK_PING_PORT:-"8081"}
+  TRAEFIK_SSL_PATH=${TRAEFIK_SSL_PATH:-"$(pwd)/certs"}
+  TRAEFIK_ACME_ENABLE=${TRAEFIK_ACME_ENABLE:-"false"}
+  TRAEFIK_ACME_EMAIL=${TRAEFIK_ACME_EMAIL:-"test@traefik.io"}
+  TRAEFIK_ACME_STORAGE=${TRAEFIK_ACME_STORAGE:-"$(pwd)/acme/acme.json"}
+  TRAEFIK_ACME_ONHOSTRULE=${TRAEFIK_ACME_ONHOSTRULE:-"false"}
+  TRAEFIK_ACME_CASERVER=${TRAEFIK_ACME_CASERVER:-"https://acme-v01.api.letsencrypt.org/directory"}
+  TRAEFIK_ACME_DCOS_DOMAIN=$TRAEFIK_DCOS_CLUSTER_NAME.$TRAEFIK_CONSUL_DOMAIN
+  TRAEFIK_ACME_DNS_PROVIDER=${TRAEFIK_ACME_DNS_PROVIDER:-"-"}
+  TRAEFIK_K8S_ENABLE=${TRAEFIK_K8S_ENABLE:-"false"}
+  TRAEFIK_K8S_OPTS=${TRAEFIK_K8S_OPTS:-""}
+  TRAEFIK_PROMETHEUS_ENABLE=${TRAEFIK_PROMETHEUS_ENABLE:-"false"}
+  TRAEFIK_PROMETHEUS_OPTS=${TRAEFIK_PROMETHEUS_OPTS:-""}
+  TRAEFIK_PROMETHEUS_ENTRYPOINT=${TRAEFIK_PROMETHEUS_ENTRYPOINT:-"traefik"}
+  TRAEFIK_PROMETHEUS_BUCKETS=${TRAEFIK_PROMETHEUS_BUCKETS:-"[0.1,0.3,1.2,5.0]"}
+  TRAEFIK_RANCHER_ENABLE=${TRAEFIK_RANCHER_ENABLE:-"false"}
+  TRAEFIK_RANCHER_REFRESH=${TRAEFIK_RANCHER_REFRESH:-15}
+  TRAEFIK_RANCHER_MODE=${TRAEFIK_RANCHER_MODE:-"api"}
+  TRAEFIK_RANCHER_DOMAIN=${TRAEFIK_RANCHER_DOMAIN:-"rancher.internal"}
+  TRAEFIK_RANCHER_EXPOSE=${TRAEFIK_RANCHER_EXPOSE:-"false"}
+  TRAEFIK_RANCHER_HEALTHCHECK=${TRAEFIK_RANCHER_HEALTHCHECK:-"true"}
+  TRAEFIK_RANCHER_INTERVALPOLL=${TRAEFIK_RANCHER_INTERVALPOLL:-"false"}
+  TRAEFIK_RANCHER_OPTS=${TRAEFIK_RANCHER_OPTS:-""}
+  TRAEFIK_RANCHER_PREFIX=${TRAEFIK_RANCHER_PREFIX:-"/2017-11-11"}
+  TRAEFIK_FILE_NAME=${TRAEFIK_FILE_NAME:-"rules.toml"}
+  TRAEFIK_FILE_WATCH=${TRAEFIK_FILE_WATCH:="true"}
+  CATTLE_URL=${CATTLE_URL:-""}
+  CATTLE_ACCESS_KEY=${CATTLE_ACCESS_KEY:-""}
+  CATTLE_SECRET_KEY=${CATTLE_SECRET_KEY:-""}
+  TRAEFIK_MARATHON_ENABLE=${TRAEFIK_MARATHON_ENABLE:-"true"}
+  TRAEFIK_MARATHON_ENDPOINT=${TRAEFIK_MARATHON_ENDPOINT:-"http://marathon.mesos:8080"}
+  TRAEFIK_MARATHON_WATCH=${TRAEFIK_MARATHON_WATCH:-"true"}
+  TRAEFIK_MARATHON_LB_COMPATIBILITY=${TRAEFIK_MARATHON_LB_COMPATIBILITY:-"false"}
+  TRAEFIK_MARATHON_DOMAIN=$TRAEFIK_DCOS_CLUSTER_NAME.$TRAEFIK_CONSUL_DOMAIN
+  TRAEFIK_MARATHON_OPTS=${TRAEFIK_MARATHON_OPTS:-""}
+  TRAEFIK_MARATHON_EXPOSE=${TRAEFIK_MARATHON_EXPOSE:-"true"}
+  TRAEFIK_MARATHON_GROUPS_AS_SUBDOMAINS=${TRAEFIK_MARATHON_GROUPS_AS_SUBDOMAINS:-"false"}
+  TRAEFIK_MARATHON_DIALER_TIMEOUT=${TRAEFIK_MARATHON_DIALER_TIMEOUT:-"60s"}
+  TRAEFIK_MARATHON_KEEP_ALIVE=${TRAEFIK_MARATHON_KEEP_ALIVE:-"10s"}
+  TRAEFIK_MARATHON_FORCE_TASK_HOSTNAME=${TRAEFIK_MARATHON_FORCE_TASK_HOSTNAME:-"false"}
+  TRAEFIK_MARATHON_RESPECT_READINESS_CHECKS=${TRAEFIK_MARATHON_RESPECT_READINESS_CHECKS:-"false"}
+
+TRAEFIK_ENTRYPOINTS_HTTP="\
+  [entryPoints.http]
+  address = \"${TRAEFIK_HTTP_ADDRESS}:${TRAEFIK_HTTP_PORT}\"
+  compress = ${TRAEFIK_HTTP_COMPRESSION}
+"
+
+if [ "X${TRAEFIK_HTTPS_ENABLE}" == "Xtrue" ]; then
+  TRAEFIK_ENTRYPOINTS_HTTPS="\
+  [entryPoints.https]
+  address = \"${TRAEFIK_HTTPS_ADDRESS}:${TRAEFIK_HTTPS_PORT}\"
+  compress = ${TRAEFIK_HTTPS_COMPRESSION}
+    [entryPoints.https.tls]
+"
+
+  if [ -d "${TRAEFIK_SSL_PATH}" ]; then
+    filelist=`ls -1 ${TRAEFIK_SSL_PATH}/*.key | rev | cut -d"." -f2- | rev`
+    RC=`echo $?`
+
+    if [ $RC -eq 0 ]; then
+      for i in $filelist; do
+        if [ -f "$i.crt" ]; then
+          TRAEFIK_ENTRYPOINTS_HTTPS=$TRAEFIK_ENTRYPOINTS_HTTPS"
+      [[entryPoints.https.tls.certificates]]
+      certFile = \"${i}.crt\"
+      keyFile = \"${i}.key\"
+"
+        fi
+      done
+    fi
+  fi
+
+  if [ "X${TRAEFIK_HTTPS_REDIRECT_ENABLE}" == "Xtrue" ]; then
+    TRAEFIK_ENTRYPOINTS_HTTP=$TRAEFIK_ENTRYPOINTS_HTTP"\
+    [entryPoints.http.redirect]
+       entryPoint = \"https\"
+"
+  fi
+
+  TRAEFIK_ENTRYPOINTS_OPTS=${TRAEFIK_ENTRYPOINTS_HTTP}${TRAEFIK_ENTRYPOINTS_HTTPS}
+  TRAEFIK_ENTRYPOINTS='"http", "https"'
+else
+  TRAEFIK_ENTRYPOINTS_OPTS=${TRAEFIK_ENTRYPOINTS_HTTP}
+  TRAEFIK_ENTRYPOINTS='"http"'
+fi
+
+if [ "X${TRAEFIK_API_ENABLE}" == "Xtrue" ]; then
+  TRAEFIK_ENTRYPOINTS_API="\
+  [entryPoints.traefik]
+  address = \":${TRAEFIK_API_PORT}\"
+"
+    if [ "${TRAEFIK_API_AUTH_USERS}" != "" ]; then
+      echo ${TRAEFIK_API_AUTH_USERS} > "$(pwd)/.htpasswd"
+      TRAEFIK_ENTRYPOINTS_API=$TRAEFIK_ENTRYPOINTS_API"\
+    [entryPoints.traefik.auth]
+      [entryPoints.traefik.auth.${TRAEFIK_API_AUTH_METHOD}]
+      usersFile = \"$(pwd)/.htpasswd\"
+"
+    fi
+  TRAEFIK_ENTRYPOINTS_OPTS=${TRAEFIK_ENTRYPOINTS_OPTS}${TRAEFIK_ENTRYPOINTS_API}
+fi
+
+TRAEFIK_ENTRYPOINTS_PING="\
+  [entryPoints.ping]
+  address = \":${TRAEFIK_PING_PORT}\"
+"
+TRAEFIK_ENTRYPOINTS_OPTS=${TRAEFIK_ENTRYPOINTS_OPTS}${TRAEFIK_ENTRYPOINTS_PING}
+
+if [ "X${TRAEFIK_K8S_ENABLE}" == "Xtrue" ]; then
+    TRAEFIK_K8S_OPTS="[kubernetes]"
+fi
+
+if [ "X${TRAEFIK_HTTPS_ENABLE}" == "Xtrue" ] && [ "X${TRAEFIK_ACME_ENABLE}" == "Xtrue" ]; then
+  TRAEFIK_ACME_CFG="\
+
+[acme]
+email = \"${TRAEFIK_ACME_EMAIL}\"
+storage = \"${TRAEFIK_ACME_STORAGE}\"
+onHostRule = ${TRAEFIK_ACME_ONHOSTRULE}
+caServer = \"${TRAEFIK_ACME_CASERVER}\"
+entryPoint = \"https\"
+  [acme.dnsChallenge]
+    provider = \"${TRAEFIK_ACME_DNS_PROVIDER}\"
+  [[acme.domains]]
+    main = \"*.${TRAEFIK_ACME_DCOS_DOMAIN}\"
+  [[acme.domains]]
+    main = \"${TRAEFIK_ACME_DCOS_DOMAIN}\"
+"
+fi
+
+if [ "X${TRAEFIK_RANCHER_ENABLE}" == "Xtrue" ]; then
+    TRAEFIK_RANCHER_OPTS="\
+[rancher]
+domain = \"${TRAEFIK_RANCHER_DOMAIN}\"
+Watch = true
+RefreshSeconds = ${TRAEFIK_RANCHER_REFRESH}
+ExposedByDefault = ${TRAEFIK_RANCHER_EXPOSE}
+EnableServiceHealthFilter = ${TRAEFIK_RANCHER_HEALTHCHECK}
+"
+    if [ "${TRAEFIK_RANCHER_MODE}" == "api" ]; then
+        TRAEFIK_RANCHER_OPTS=${TRAEFIK_RANCHER_OPTS}"
+[rancher.api]
+Endpoint = \"${CATTLE_URL}\"
+AccessKey = \"${CATTLE_ACCESS_KEY}\"
+SecretKey = \"${CATTLE_SECRET_KEY}\"
+"
+    elif [ "${TRAEFIK_RANCHER_MODE}" == "metadata" ]; then
+        TRAEFIK_RANCHER_OPTS=${TRAEFIK_RANCHER_OPTS}"
+[rancher.metadata]
+IntervalPoll = ${TRAEFIK_RANCHER_INTERVALPOLL}
+Prefix = \"${TRAEFIK_RANCHER_PREFIX}\"
+"
+    fi
+fi
+
+if [ "X${TRAEFIK_API_ENABLE}" == "Xtrue" ]; then
+    TRAEFIK_API_CFG="\
+
+[api]
+entryPoint = \"traefik\"
+dashboard = true
+debug = ${TRAEFIK_DEBUG}
+  [api.statistics]
+  recentErrors = ${TRAEFIK_STATISTICS_RECENT_ERRORS}
+"
+fi
+
+TRAEFIK_PING_CFG="\
+
+[ping]
+entryPoint = \"ping\"
+"
+
+# Metrics definition
+if [ "X${TRAEFIK_PROMETHEUS_ENABLE}" == "Xtrue" ]; then
+    TRAEFIK_PROMETHEUS_OPTS="\
+[metrics]
+  [metrics.prometheus]
+  entryPoint = \"${TRAEFIK_PROMETHEUS_ENTRYPOINT}\"
+  # Buckets for latency metrics
+  #
+  # Optional
+  # Default: [0.1, 0.3, 1.2, 5]
+  #
+  buckets=${TRAEFIK_PROMETHEUS_BUCKETS}
+"
+fi
+
+if [ -f "${TRAEFIK_FILE_NAME}" ]; then
+    TRAEFIK_FILE_OPTS="\
+
+[file]
+filename = \"${TRAEFIK_FILE_NAME}\"
+watch = ${TRAEFIK_FILE_WATCH}
+"
+fi
+
+if [ "X${TRAEFIK_MARATHON_ENABLE}" == "Xtrue" ]; then
+    TRAEFIK_MARATHON_OPTS="\
+
+[marathon]
+# Marathon server endpoint.
+# You can also specify multiple endpoint for Marathon:
+# endpoint = \"http://10.241.1.71:8080,10.241.1.72:8080,10.241.1.73:8080\"
+#
+endpoint = \"${TRAEFIK_MARATHON_ENDPOINT}\"
+watch = ${TRAEFIK_MARATHON_WATCH}
+
+# Default domain used.
+# Can be overridden by setting the \"traefik.domain\" label on an application.
+#
+# Required
+#
+domain = \"${TRAEFIK_MARATHON_DOMAIN}\"
+
+# Expose Marathon apps by default in Traefik.
+#
+# Optional
+# Default: true
+#
+exposedByDefault = ${TRAEFIK_MARATHON_EXPOSE}
+
+# Convert Marathon groups to subdomains.
+# Default behavior: /foo/bar/myapp => foo-bar-myapp.{defaultDomain}
+# with groupsAsSubDomains enabled: /foo/bar/myapp => myapp.bar.foo.{defaultDomain}
+#
+# Optional
+# Default: false
+#
+groupsAsSubDomains = ${TRAEFIK_MARATHON_GROUPS_AS_SUBDOMAINS}
+
+# Enable compatibility with marathon-lb labels.
+#
+# Optional
+# Default: false
+#
+marathonLBCompatibility = ${TRAEFIK_MARATHON_LB_COMPATIBILITY}
+
+# Enable Marathon basic authentication.
+#
+# Optional
+#
+#    [marathon.basic]
+#    httpBasicAuthUser = \"foo\"
+#    httpBasicPassword = \"bar\"
+
+# TLS client configuration. https://golang.org/pkg/crypto/tls/#Config
+#
+# Optional
+#
+#    [marathon.TLS]
+#    CA = \"/etc/ssl/ca.crt\"
+#    Cert = \"/etc/ssl/marathon.cert\"
+#    Key = \"/etc/ssl/marathon.key\"
+#    InsecureSkipVerify = true
+
+# DCOSToken for DCOS environment.
+# This will override the Authorization header.
+#
+# Optional
+#
+# dcosToken = \"xxxxxx\"
+
+# Override DialerTimeout.
+# Amount of time to allow the Marathon provider to wait to open a TCP connection
+# to a Marathon master.
+# Can be provided in a format supported by [time.ParseDuration](https://golang.org/pkg/time/#ParseDuration) or as raw
+# values (digits).
+# If no units are provided, the value is parsed assuming seconds.
+#
+# Optional
+# Default: \"60s\"
+#
+dialerTimeout = \"${TRAEFIK_MARATHON_DIALER_TIMEOUT}\"
+
+# Set the TCP Keep Alive interval for the Marathon HTTP Client.
+# Can be provided in a format supported by [time.ParseDuration](https://golang.org/pkg/time/#ParseDuration) or as raw
+# values (digits).
+# If no units are provided, the value is parsed assuming seconds.
+#
+# Optional
+# Default: \"10s\"
+#
+keepAlive = \"${TRAEFIK_MARATHON_KEEP_ALIVE}\"
+
+# By default, a task's IP address (as returned by the Marathon API) is used as
+# backend server if an IP-per-task configuration can be found; otherwise, the
+# name of the host running the task is used.
+# The latter behavior can be enforced by enabling this switch.
+#
+# Optional
+# Default: false
+#
+forceTaskHostname = ${TRAEFIK_MARATHON_FORCE_TASK_HOSTNAME}
+
+# Applications may define readiness checks which are probed by Marathon during
+# deployments periodically and the results exposed via the API.
+# Enabling the following parameter causes Traefik to filter out tasks
+# whose readiness checks have not succeeded.
+# Note that the checks are only valid at deployment times.
+# See the Marathon guide for details.
+#
+# Optional
+# Default: false
+#
+respectReadinessChecks = ${TRAEFIK_MARATHON_RESPECT_READINESS_CHECKS}
+"
+fi
+
+local opts=""
+  if [ ! -z "${TRAEFIK_ENTRYPOINTS_OPTS}" ]; then
+    opts+="${TRAEFIK_ENTRYPOINTS_OPTS}"
+  fi
+  if [ ! -z "${TRAEFIK_API_CFG}" ]; then
+    opts+="${TRAEFIK_API_CFG}"
+  fi
+  if [ ! -z "${TRAEFIK_PING_CFG}" ]; then
+    opts+="${TRAEFIK_PING_CFG}"
+  fi
+  if [ ! -z "${TRAEFIK_PROMETHEUS_OPTS}" ]; then
+    opts+="${TRAEFIK_PROMETHEUS_OPTS}"
+  fi
+  if [ ! -z "${TRAEFIK_RANCHER_OPTS}" ]; then
+    opts+="${TRAEFIK_RANCHER_OPTS}"
+  fi
+  if [ ! -z "${TRAEFIK_FILE_OPTS}" ]; then
+    opts+="${TRAEFIK_FILE_OPTS}"
+  fi
+  if [ ! -z "${TRAEFIK_ACME_CFG}" ]; then
+    opts+="${TRAEFIK_ACME_CFG}"
+  fi
+  if [ ! -z "${TRAEFIK_K8S_OPTS}" ]; then
+    opts+="${TRAEFIK_K8S_OPTS}"
+  fi
+  if [ ! -z "${TRAEFIK_MARATHON_OPTS}" ]; then
+    opts+="${TRAEFIK_MARATHON_OPTS}"
+  fi
+
+
+cat << EOF > ./traefik.toml
+# traefik.toml
+logLevel = "${TRAEFIK_LOG_LEVEL}"
+InsecureSkipVerify = ${TRAEFIK_INSECURE_SKIP}
+defaultEntryPoints = [${TRAEFIK_ENTRYPOINTS}]
+
+[entryPoints]
+${opts}
+EOF
+}
+
 
 if [[ ${1:-} ]] && declare -F | cut -d' ' -f3 | fgrep -qx -- "${1:-}"
 then
